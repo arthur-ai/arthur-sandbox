f72ee0406c211a2084871b745d0ff1b8ae35308a
Keegan Hines <keegan@Keegans-MacBook-Air.local>
2020-10-11 16:57:02 -0400

    update boston

--- example_projects/boston_housing_spark_model/upload_model.py
+++ example_projects/boston_housing_spark_model/upload_model.py
@@ -1,12 +1,11 @@
 from arthurai import ArthurAI
-from arthurai import ModelType, InputType, Stage, DataType, ArthurModel
-from arthurai.client.apiv2.arthur_explainer import ArthurExplainer
+from arthurai.client.apiv3 import OutputType, InputType, Stage, ValueType, AttributeCategory
 from pyspark.sql import SparkSession
 
 
 spark = SparkSession.builder.appName('app').getOrCreate()
 
-client = ArthurAI(url='dashboard.arthur.ai', access_key='<access_key>')
+client = ArthurAI(url='app.arthur.ai', access_key='<access_key>')
 
 # create a dataframe from the training data to on-board model metadata
 train = spark.read.parquet("./data/train.parquet")
@@ -19,24 +18,24 @@ MODEL_METADATA = {
     "name": 'boston_housing_model',
     "description": "Spark Boston Housing Model",
     "input_type": InputType.Tabular,
-    "model_type": ModelType.Regression,
+    "model_type": OutputType.Regression,
     "tags": ['Spark'],
     "is_batch": True
 }
 
 model = client.model(**MODEL_METADATA)
 model.from_dataframe(train_df[list(train_df.columns)[0:]], Stage.ModelPipelineInput)
-model.attribute(
-    name='medv',
+model.add_attribute(
+    name='medv_gt',
     stage=Stage.GroundTruth,
     data_type=DataType.Float,
     categorical=False,
-    position=0
+    attribute_link="medv_prediction"
 )
+
+model.save()
 
 model.enable_explainability(df=train_df, project_directory='.',
                             user_predict_function_import_path='entrypoint',
                             requirements_file='requirements.txt',
-                            model_server_num_cpu='2')
-
-model.save()
+                            model_server_num_cpu='2', model_server_memory='1Gi')
